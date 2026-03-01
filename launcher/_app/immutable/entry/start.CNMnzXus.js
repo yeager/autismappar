9311878f7c689b3db0316b552da60c6027422ec1
Daniel Nylander <po@danielnylander.se>
2026-03-01 15:34:41 +0100

    deploy: SW SPA fallback fix for GitHub Pages routing

---
+++ launcher/_app/immutable/entry/start.CNMnzXus.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/Gs33YBRw.js";import{w as t}from"../chunks/ELmspNgw.js";export{t as load_css,r as start};
--- launcher/_app/immutable/entry/start.DwhdjEaU.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/C7HAlxDp.js";import{w as t}from"../chunks/D4OKE1ve.js";export{t as load_css,r as start};
