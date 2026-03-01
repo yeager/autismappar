ae22f6e10a16728852c932079b867283005467c3
Daniel Nylander <po@danielnylander.se>
2026-03-01 15:08:48 +0100

    launcher: rebuild with base path fix for icons and hero image

---
+++ launcher/_app/immutable/entry/start.CJkSghzC.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/BIzAMtDO.js";import{w as t}from"../chunks/75CBJPf5.js";export{t as load_css,r as start};
--- launcher/_app/immutable/entry/start.D3T-XEwj.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/DQRTMWXO.js";import{w as t}from"../chunks/dRyt2hJh.js";export{t as load_css,r as start};
