0361c10393710266f26044dec7babdf0bfc31abf
Daniel Nylander <po@danielnylander.se>
2026-03-01 16:13:53 +0100

    deploy: launcher with base path fixes for goto() and ONNX WASM

---
+++ launcher/_app/immutable/entry/start.B283fmJ2.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/BfatAOz4.js";import{w as t}from"../chunks/DI48-JTw.js";export{t as load_css,r as start};
--- launcher/_app/immutable/entry/start.g9zn698V.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/BVddcIhQ.js";import{w as t}from"../chunks/gqc4WGPV.js";export{t as load_css,r as start};
