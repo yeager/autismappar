c4bfe236e76d5b02905a6f420983459583bd9247
Daniel Nylander <po@danielnylander.se>
2026-03-01 15:53:59 +0100

    Update launcher: language switcher in header

--- launcher/_app/immutable/entry/start.DYYX_lQb.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/Cpqx1-RK.js";import{w as t}from"../chunks/C_VZ-11v.js";export{t as load_css,r as start};
---
+++ launcher/_app/immutable/entry/start.g9zn698V.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/BVddcIhQ.js";import{w as t}from"../chunks/gqc4WGPV.js";export{t as load_css,r as start};
