d2d88831ca58754c0fd5468f46c0d6ee284870c7
Daniel Nylander <po@danielnylander.se>
2026-03-01 15:23:06 +0100

    Fix all 404s: SPA redirect pickup, sagostunden.html, directory redirects for extensionless URLs
    - Rebuild launcher with SPA redirect pickup script in app.html
    - Add sagostunden.html app page
    - Create apps/<id>/index.html redirects for all 60 apps (extensionless URL support)

--- launcher/_app/immutable/entry/start.CJkSghzC.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/BIzAMtDO.js";import{w as t}from"../chunks/75CBJPf5.js";export{t as load_css,r as start};
---
+++ launcher/_app/immutable/entry/start.zw1B50JV.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/BcDuwHRi.js";import{w as t}from"../chunks/Bix8baaV.js";export{t as load_css,r as start};
