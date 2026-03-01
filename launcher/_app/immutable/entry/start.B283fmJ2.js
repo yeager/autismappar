d9bef5cebc2980051933b56772d51a203fa47cd7
Daniel Nylander <po@danielnylander.se>
2026-03-01 16:15:07 +0100

    deploy: layout base path fix

--- launcher/_app/immutable/entry/start.B283fmJ2.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/BfatAOz4.js";import{w as t}from"../chunks/DI48-JTw.js";export{t as load_css,r as start};
---
+++ launcher/_app/immutable/entry/start.ChkFy2-i.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/BMTvbAH8.js";import{w as t}from"../chunks/B0fiZRqZ.js";export{t as load_css,r as start};
