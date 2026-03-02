2e62513c5e50c2b1c86938642578df0e43ed4455
Daniel Nylander <daniel@danielnylander.se>
2026-03-02 09:01:26 +0100

    Deploy: fix long app name wrapping

---
+++ _app/immutable/entry/start.BFkXWsKm.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/DcUeCnm3.js";import{w as t}from"../chunks/BqWe3cL5.js";export{t as load_css,r as start};
--- _app/immutable/entry/start.BlJRhmPZ.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/Cth-fmii.js";import{w as t}from"../chunks/C3CNpY5x.js";export{t as load_css,r as start};
