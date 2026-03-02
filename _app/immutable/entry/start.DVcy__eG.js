eafd57addee1b6c06bb5ecccd51ebf4923a30664
Daniel Nylander <daniel@danielnylander.se>
2026-03-02 07:20:31 +0100

    Deploy: fix blank page (BASE_PATH fix for root deployment)

---
+++ _app/immutable/entry/start.DVcy__eG.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/DornHELJ.js";import{w as t}from"../chunks/D_KoUcwb.js";export{t as load_css,r as start};
--- _app/immutable/entry/start.Dz9vG9Sn.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/I97kRy2W.js";import{w as t}from"../chunks/DOdnC0Nz.js";export{t as load_css,r as start};
