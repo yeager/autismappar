8a32c9fa2a2fc879e0c701193e05b0595d271f8d
Daniel Nylander <daniel@danielnylander.se>
2026-03-02 09:49:23 +0100

    Deploy: fix SW accepting GitHub Pages 404 SPA responses

--- _app/immutable/entry/start.5cRpIpK1.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/DQlPAET0.js";import{w as t}from"../chunks/B4fKfxel.js";export{t as load_css,r as start};
---
+++ _app/immutable/entry/start.WZAtqcDM.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/DYe2K0Qg.js";import{w as t}from"../chunks/Dld2IQBs.js";export{t as load_css,r as start};
