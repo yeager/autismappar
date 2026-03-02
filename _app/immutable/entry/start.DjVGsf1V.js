e73209acb3935fed5b520531d7ab348366a1f60a
Daniel Nylander <daniel@danielnylander.se>
2026-03-02 07:34:21 +0100

    Deploy: fix language dropdown on iPhone

--- _app/immutable/entry/start.DjVGsf1V.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/DxQnMi92.js";import{w as t}from"../chunks/By20y5Gr.js";export{t as load_css,r as start};
---
+++ _app/immutable/entry/start.KlJvJcfr.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/BWNaJ52P.js";import{w as t}from"../chunks/b-Zncsv0.js";export{t as load_css,r as start};
